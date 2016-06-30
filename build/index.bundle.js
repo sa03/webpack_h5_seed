8834a658ddd2d6882f6c53b8442e6bf7b705dff7
sa03 <303914098@qq.com>
2016-06-30 15:10:47 +0800

    debug

--- build/index.bundle.js
+++ build/index.bundle.js
@@ -5,39 +5,235 @@ webpackJsonp([0],[
 	"use strict";
 
 	var $ = __webpack_require__(1);
-	var lib = __webpack_require__(7);
+	var lib = __webpack_require__(2);
 
-	__webpack_require__(2);
-	__webpack_require__(5);
+	__webpack_require__(4);
+	__webpack_require__(13);
 
 	$(function () {
-		window.loadReady = true;
+		setTimeout(function () {
+			window.loadReady = true;
+		}, 1000);
+		var apiDomain = "";
+		if (/webdev/.test(location.origin)) {
+			apiDomain = "http://test.www.hiyd.com";
+		} else {
+			apiDomain = location.origin.replace("m.hiyd.com", "www.hiyd.com");
+		}
+
+		var T = {
+			checkForm: function checkForm() {
+				var data = {};
+
+				T.initError();
+				if (!T.checkPhone()) {
+					return false;
+				}
+				if (!T.checkVCode()) {
+					return false;
+				}
+				if (!T.checkQQ()) {
+					return false;
+				}
+				data.mobile = $("#phone").val();
+				data.qq = $("#qq").val();
+				data.vCode = $("#vCode").val();
+
+				T.initError();
+				return data;
+			},
+			checkPhone: function checkPhone() {
+				var reg = /\d{11}/;
+				var phone = $("#phone").val();
+
+				if (!reg.test(phone)) {
+					$('#phone-error').text('手机格式错误');
+					return false;
+				}
+				return true;
+			},
+			checkVCode: function checkVCode() {
+				var reg = /\d{4}/;
+				var vCode = $("#vCode").val();
+				if (!reg.test(vCode)) {
+					$('#vCode-error').text('请输入4位验证码');
+					return false;
+				}
+				return true;
+			},
+			checkQQ: function checkQQ() {
+				var reg = /\d{5,12}/;
+				var qq = $("#qq").val();
+
+				if (!reg.test(qq)) {
+					$('#qq-error').text('qq填写错误');
+					return false;
+				}
+				return true;
+			},
+			sendVCode: function sendVCode() {
+				var data = {};
+				var url = apiDomain + '/account/sendVCode2';
+
+				data.mobile = $("#phone").val();
+				$.ajax({
+					type: "post",
+					async: false,
+					url: url,
+					data: data,
+					// jsonp: "callbackparam",//传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(默认为:callback)
+					// jsonpCallback:"success_jsonpCallback",//自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
+					success: function success(result) {
+						if (result.code == 0) {
+							$('#phone-error').text('');
+							lib.showDialog("验证码已发送,请注意查收");
+							V.countVCode(60);
+						} else {
+							lib.showDialog(result.msg);
+						}
+					}
+				});
+			},
+			checkMobile: function checkMobile() {
+				var data = {};
+				var phone = $("#phone").val();
+				var reg = /\d{11}/;
+				var url = apiDomain + '/account/checkMobile';
+				if (!reg.test(phone)) {
+					$('#phone-error').text('手机格式错误');
+					return;
+				}
+				$('#phone-error').text('');
+				data.mobile = phone;
+				$.ajax({
+					type: "get",
+					async: false,
+					url: url,
+					data: data,
+					jsonp: "callbackparam", //传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(默认为:callback)
+					jsonpCallback: "success_jsonpCallback", //自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名
+					success: function success(result) {
+						if (result.code == 0) {
+							T.sendVCode();
+						} else {
+							lib.showDialog(result.msg);
+						}
+					},
+					error: function error(json) {
+						console.log(json);
+					}
+				});
+			},
+			submitBox: function submitBox() {
+				var url = "/activity/box";
+				var data = T.checkForm();
+				if (!data) {
+					return;
+				}
+				$.post(url, data, function (result) {
+					if (result.code == 0) {
+						$("#login-form").hide();
+						$("#qq-code").text(data.qq);
+						$("#finish-wrap").show();
+					} else {
+						lib.showDialog(result.msg);
+					}
+				});
+			},
+			initForm: function initForm() {
+				$("#login-wrap").css({
+					'-webkit-transform': 'translate(-50%,-50%)',
+					'transform': 'translate(-50%,-50%)',
+					'top': '50%'
+				});
+			},
+			initError: function initError() {
+				$('#phone-error').text('');
+				$('#vCode-error').text('');
+				$('#qq-error').text('');
+			},
+			closeForm: function closeForm() {
+				$('#phone-error').text('');
+				$('#vCode-error').text('');
+				$('#qq-error').text('');
+				T.initForm();
+				$("#login-mask").hide();
+				$("#login-wrap").hide();
+			}
+		};
+
+		var V = {
+			init: function init() {
+				var ObjMask = $("#login-mask");
+				var ObjForm = $("#login-wrap");
+
+				$("input[type='text'],input[type='tel']").focus(function () {
+					$("#login-wrap").css({
+						'-webkit-transform': 'translate(-50%,0)',
+						'transform': 'translate(-50%,0)',
+						'top': '1.2rem'
+					});
+				}).blur(function () {
+					T.initForm();
+				});
+				$("#btn-login").on('click', function () {
+					$("#phone").val('');
+					$("#qq").val('');
+					$("#vCode").val('');
+
+					$("#finish-wrap").hide();
+					$("#login-form").show();
+					ObjForm.show();
+					ObjMask.show();
+				});
+
+				ObjForm.on('click', '#getVCode', function () {
+					if (!$(this).hasClass("disable")) {
+						T.checkMobile();
+					}
+				});
+				ObjMask.on('click', function () {
+					T.closeForm();
+				});
+
+				ObjForm.on('click', '#login', function () {
+					T.submitBox();
+				});
+
+				ObjForm.on('click', '#btn-ok', function () {
+					T.closeForm();
+				});
+				$("#close-form").on('click', function () {
+					T.closeForm();
+				});
+			},
+			countVCode: function countVCode(seconds) {
+				var timer = seconds;
+				var st = setInterval(function () {
+					if (timer < 0) {
+						$("#getVCode").text("获取验证码").removeClass("disable");
+						clearTimeout(st);
+					} else {
+						$("#getVCode").addClass("disable");
+						$("#getVCode").text(timer + "秒");
+						timer -= 1;
+					}
+				}, 1000);
+			}
+		};
+
+		V.init();
 	});
 
 /***/ },
 /* 1 */,
 /* 2 */
-/***/ function(module, exports) {
-
-	// removed by extract-text-webpack-plugin
-
-/***/ },
-/* 3 */,
-/* 4 */,
-/* 5 */
-/***/ function(module, exports) {
-
-	// removed by extract-text-webpack-plugin
-
-/***/ },
-/* 6 */,
-/* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
 
 	var $ = __webpack_require__(1);
-	__webpack_require__(8);
+	__webpack_require__(3);
 	var loadingDelayHandler = 0;
 	var loadingTimeoutHandler = 0;
 	var loadDialog;
@@ -258,14 +454,48 @@ webpackJsonp([0],[
 	        } else {
 	            return false;
 	        }
+	    },
+	    showDialog: function showDialog(msg, callback, title, buttonLabels) {
+	        title = title || "提示";
+	        buttonLabels = buttonLabels || "确定";
+	        var d = dialog({
+	            title: title,
+	            content: msg,
+	            skin: 'weui-dialog',
+	            okValue: buttonLabels,
+	            ok: function ok() {
+	                callback && callback(true);
+	                d.close().remove();
+	            }
+	        });
+	        d.width(window.innerWidth * 0.75).showModal();
+	    },
+	    confirm: function confirm(msg, callback, title, buttonLabels) {
+	        title = title || "提示";
+	        buttonLabels = buttonLabels || "确定,取消";
+	        var d = dialog({
+	            title: title,
+	            content: msg,
+	            skin: 'weui-dialog',
+	            okValue: buttonLabels.split(",")[0],
+	            ok: function ok() {
+	                callback(true);
+	                d.close().remove();
+	            },
+	            cancelValue: buttonLabels.split(",")[1],
+	            cancel: function cancel() {
+	                callback(false);
+	                d.close().remove();
+	            }
+	        });
+	        d.width(window.innerWidth * 0.75).showModal();
 	    }
-
 	};
 
 	module.exports = lib;
 
 /***/ },
-/* 8 */
+/* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*! artDialog v6.0.5 | https://github.com/aui/artDialog */
@@ -1377,6 +1607,26 @@ webpackJsonp([0],[
 	    window.dialog = require("dialog");
 	})();
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 5 */,
+/* 6 */,
+/* 7 */,
+/* 8 */,
+/* 9 */,
+/* 10 */,
+/* 11 */,
+/* 12 */,
+/* 13 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
 
 /***/ }
 ]);
